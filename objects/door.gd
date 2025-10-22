238cb3ccde3fe52988494bcaacfb281f8341633e
JOÃO PEDRO SUMI BEBBER <joao.bebber@catolicasc.edu.br>
2025-10-22 20:45:05 -0300

    revert

---
+++ objects/door.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@export var scene_path:PackedScene
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		get_tree().change_scene_to_file(scene_path.resource_path)
